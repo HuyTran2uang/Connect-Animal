56836e4b80cccd336400457a9ccd0754becf27fe
binh04082202 <binh04082202@gmail.com>
2023-07-11 11:42:55 +0700

    Win Lose UI Popup

--- Assets/Scripts/Star/StarUI.cs
+++ Assets/Scripts/Star/StarUI.cs
@@ -7,6 +7,7 @@ public class StarUI : MonoBehaviour, IChangeStarText
 {
     [SerializeField] TMP_Text _starText;
     [SerializeField] TMP_Text _starTextInLevel;
+    [SerializeField] TMP_Text _starTextReceived;
 
 
     public void ChangeStarText(int quantity)
@@ -17,5 +18,8 @@ public class StarUI : MonoBehaviour, IChangeStarText
     public void ChangeStarTextInLevel(int level)
     {
         _starTextInLevel.text = level.ToString();
+        _starTextReceived.text = level.ToString();
     }
+
+
 }
